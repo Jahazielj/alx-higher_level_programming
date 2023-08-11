c6b79f8f0ecf853e0cb98a50d12062829c0cc281
“Jahazielj” <jahazieljacobs@icloud.com>
2023-08-10 22:45:28 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
-!#/usr/bin/python3
-#2-print_alphabet.py
+#!/usr/bin/python3
+# 2-print_alphabet.py
 
-""print the alphabet in lowercase, dont add a new line.""
-for letter in range (97, 123):
-print("{}".format(chr(letter)), end="")
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
